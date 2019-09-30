defe31d9ba6ad993cca3843d991e943f873f4cd4
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-30 19:32:11 +0000

    add new urls, create random function string

--- express_server.js
+++ express_server.js
@@ -44,8 +44,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  const genshortURL = generateRandomString();
-  urlDatabase[genshortURL] = req.body.longURL;
+  console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
