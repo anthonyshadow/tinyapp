3c3e0aadad0df2f67b2ded951f216ffab7f32e5d
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-30 19:31:56 +0000

    add new urls, create random function string

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+const generateRandomString = function() {
+  var input = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var randomString = '';
+  for (var i = 0; i < 6; i++) {
+    randomString += input[Math.floor(Math.random() * input.length)]
+  }
+  return randomString; 
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,6 +38,16 @@ app.get("/urls", function(req, res) {
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  const genshortURL = generateRandomString();
+  urlDatabase[genshortURL] = req.body.longURL;
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
